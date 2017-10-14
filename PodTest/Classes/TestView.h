ce275710168819f4f021d2232432d6a5f7e48bd7
wuyanji <wuyanji@o.bike>
2017-10-14 11:37:04 +0800

    12

---
+++ PodTest/Classes/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  BusinessZ_Example
+//
+//  Created by wyj on 2017/10/13.
+//  Copyright © 2017年 wuyanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ PodTest/Classes/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  BusinessZ_Example
+//
+//  Created by wyj on 2017/10/13.
+//  Copyright © 2017年 wuyanji. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
