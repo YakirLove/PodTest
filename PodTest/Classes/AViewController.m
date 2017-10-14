17df1431648dd397b80ac46cde60a3653fd3541b
wuyanji <wuyanji@o.bike>
2017-10-14 14:43:07 +0800

    123

--- PodTest/Classes/AViewController.m
+++ PodTest/Classes/AViewController.m
@@ -20,6 +20,17 @@
     self.title = @"A";
     self.view.backgroundColor = [UIColor redColor];
     
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 50)];
+    [button setTitle:@"third" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    [button addTarget:self action:@selector(zTouch:) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+-(void)zTouch:(UIButton *)button
+{
+
 }
 
 - (void)didReceiveMemoryWarning {
